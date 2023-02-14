4f604710e443901f1d29e143bf940bf063d21e39
kinjalgawaskar <gawak052@newschool.edu>
2023-02-14 00:52:13 -0500

    added scroll container in JS

--- scroll.js
+++ scroll.js
@@ -1,7 +1,5 @@
-window.addEventListener('scroll', function() {
-    var parallaxImage = document.querySelector('.parallax-image');
-    var scrollPosition = window.pageYOffset;
-    var parallaxPosition = scrollPosition * 0.5;
-    parallaxImage.style.transform = 'translate3d(' + parallaxPosition + 'px, 0, 0)';
-  });
-  
+const container = document.querySelector('.scroll-container');
+window.addEventListener('scroll', () => {
+  const scrollPercentage = window.scrollX / (document.body.scrollWidth - window.innerWidth);
+  container.scrollLeft = container.scrollWidth * scrollPercentage;
+});
