eb6cb100bad4f8c40c939bbaccfdd78f76e0e7e1
kinjalgawaskar <gawak052@newschool.edu>
2023-02-14 20:25:10 -0500

    added sticky JS to ruler & skyline

---
+++ scroll.js
@@ -0,0 +1,23 @@
+window.onscroll = function() {myFunction()};
+
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+window.onscroll = function() {myFunction()};
+
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
