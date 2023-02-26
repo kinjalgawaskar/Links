cacac49718c1465b178845e4f6bbf2dfe23a57fb
kinjalgawaskar <gawak052@newschool.edu>
2023-02-26 18:45:43 -0500

    Revert "added hover filter and bg color"
    This reverts commit a067149fce87661dd1b2c667c4668346e5dcd393.

--- assets/script.js
+++ assets/script.js
@@ -1,72 +1,30 @@
 window.arenaCallback = () => {
 	// Put your JavaScript in here!
 	// This will run after Are.na’s API returns your data.
+}
 
-  // window.onscroll = function() {myFunction()};
+window.onscroll = function() {myFunction()};
 
-// var sticky = header.offsetTop;
-let body = document.querySelector('body')
-let imageButton = document.querySelector('.image-button')
-let linksButton = document.querySelector('.links-button')
-let allImages = document.querySelector('#image-block')
-let allLinks = document.querySelector('#link-block')
+var sticky = header.offsetTop;
 
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
 
+window.onscroll = function() {myFunction()};
 
-// function myFunction() {
-//   if (window.pageYOffset > sticky) {
-//     header.classList.add("sticky");
-//   } else {
-//     header.classList.remove("sticky");
-//   }
-// }
+var sticky = header.offsetTop;
 
-// window.onscroll = function() {myFunction()};
-
-// var sticky = header.offsetTop;
-
-// function myFunction() {
-//   if (window.pageYOffset > sticky) {
-//     header.classList.add("sticky");
-//   } else {
-//     header.classList.remove("sticky");
-//   }
-// }
-
-imageButton.onclick = function() {
-  console.log('images')
-  //add all images
-  body.appendChild(allImages)
-  //remove all links
-  body.removeChild(allLinks)
-  };
-
-
-linksButton.onclick = function() {
-  console.log('links')
-  //remove all links
-  body.appendChild(allLinks)
-  //add all images
-  body.removeChild(allImages)
-  };
-
-
-// Get all the menu items
-const menuItems = document.querySelectorAll(".menu-item");
-
-// Add a click event listener to each menu item
-menuItems.forEach((item) => {
-  item.addEventListener("click", () => {
-    // Remove the active class from all menu items
-    menuItems.forEach((item) => {
-      item.classList.remove("active");
-    });
-
-    // Add the active class to the clicked menu item
-    item.classList.add("active");
-  });
-});
-
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
 }
 
 
