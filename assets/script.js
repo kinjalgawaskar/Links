643165ca08c90a59ee5ea6394a6c8fdcfdf33a6c
kinjalgawaskar <gawak052@newschool.edu>
2023-02-26 02:31:11 -0500

    edited header scale padding

---
+++ assets/script.js
@@ -0,0 +1,30 @@
+window.arenaCallback = () => {
+	// Put your JavaScript in here!
+	// This will run after Are.na’s API returns your data.
+}
+
+window.onscroll = function() {myFunction()};
+
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+window.onscroll = function() {myFunction()};
+
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+
